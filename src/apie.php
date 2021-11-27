6c682a2ac5afbee4a23439f81014a0581e711fb2
Paul <p.miescionaitis@gmail.com>
2021-11-27 11:22:01 +0200

    uploaded files from website server

---
+++ src/apie.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>MADComs - Apie mus</title>
+  <link rel="stylesheet" href="css/styles.css">
+</head>
+    <body>
+        
+    </body>
+</html>
