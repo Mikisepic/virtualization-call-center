4bd1cc16dee514ea53e5bfbc9ef47050c21e64ac
mikisepic <mikas.pup@gmail.com>
2021-11-30 00:53:33 +0200

    Fix html file layout

--- src/test/index.php
+++ src/test/index.php
@@ -20,51 +20,58 @@
       <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
         <ul class="navbar-nav">
           <li class="nav-item">
-            <a class="nav-link" href="#">Paslaugos</a>
+            <a class="nav-link" href="#paslaugos">Paslaugos</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Sprendimai</a>
+            <a class="nav-link" href="#sprendimai">Sprendimai</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Verslo Sritys</a>
+            <a class="nav-link" href="#verslo-sritys">Verslo Sritys</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Apie mus</a>
+            <a class="nav-link" href="#apie-mus">Apie mus</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Kontaktai</a>
+            <a class="nav-link" href="#kontaktai">Kontaktai</a>
           </li>
         </ul>
       </div>
     </div>
   </nav>
 
-  <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
-    <div class="carousel-indicators">
-      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  <div id="paslaugos">
+    <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+      <div class="carousel-indicators">
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+      </div>
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img src="./images/img1.jpg" class="d-block w-100">
+        </div>
+        <div class="carousel-item">
+          <img src="./images/img2.jpg" class="d-block w-100">
+        </div>
+        <div class="carousel-item">
+          <img src="./images/img3.jpg" class="d-block w-100">
+        </div>
+      </div>
+      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
     </div>
-    <div class="carousel-inner">
-      <div class="carousel-item active">
-        <img src="./images/img1.jpg" class="d-block w-100">
-      </div>
-      <div class="carousel-item">
-        <img src="./images/img2.jpg" class="d-block w-100">
-      </div>
-      <div class="carousel-item">
-        <img src="./images/img3.jpg" class="d-block w-100">
-      </div>
-    </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Previous</span>
-    </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Next</span>
-    </button>
   </div>
+
+  <div id="sprendimai"></div>
+  <div id="verslo-sritys"></div>
+  <div id="apie-mus"></div>
+  <div id="kontaktai"></div>
 
   <a href="https://wa.me/8569541244" class="whatsapp_float" target="_blank"><i class="fa fa-whatsapp whatsapp-icon"></i></a>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
