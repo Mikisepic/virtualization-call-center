2fc8f2eab42bd4b446548e968cfe3907ab1fc7c8
mikisepic <mikas.pup@gmail.com>
2021-11-30 00:24:17 +0200

    Reconfig formatting

--- src/test/index.php
+++ src/test/index.php
@@ -1,42 +1,45 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <script src="animations.js"></script>
-        <title>MADComs</title>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-        <link rel="stylesheet" href="/css/styles.css">
-    </head>
-    <body>
-        <nav class="navbar navbar-expand-md navbar-dark">
-        <div class="container">
-            <a class="navbar-brand">MADComms</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                    <a class="nav-link" href="#">Paslaugos</a>
-                    </li>
-                    <li class="nav-item">
-                    <a class="nav-link" href="#">Sprendimai</a>
-                    </li>
-                    <li class="nav-item">
-                    <a class="nav-link" href="#">Verslo Sritys</a>
-                    </li>
-                    <li class="nav-item">
-                    <a class="nav-link" href="#">Apie mus</a>
-                    </li>
-                    <li class="nav-item">
-                    <a class="nav-link" href="#">Kontaktai</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-        </nav>
-        <a href="https://wa.me/8569541244" class="whatsapp_float" target="_blank"><i class="fa fa-whatsapp whatsapp-icon"></i></a>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-    </body>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <script src="animations.js"></script>
+  <title>MADComs</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <link rel="stylesheet" href="/css/styles.css">
+</head>
+
+<body>
+  <nav class="navbar navbar-expand-md navbar-dark">
+    <div class="container">
+      <a class="navbar-brand">MADComms</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" href="#">Paslaugos</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Sprendimai</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Verslo Sritys</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Apie mus</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Kontaktai</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <a href="https://wa.me/8569541244" class="whatsapp_float" target="_blank"><i class="fa fa-whatsapp whatsapp-icon"></i></a>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+
 </html>
