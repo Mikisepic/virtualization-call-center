10280b1f6672191f072e2448012365f677e6c5d9
Paul <p.miescionaitis@gmail.com>
2021-12-01 19:10:17 +0200

    created contact form, footer

--- src/test/index.php
+++ src/test/index.php
@@ -1,17 +1,19 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 
 <head>
+  <!-- Required meta tags -->
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <script src="animations.js"></script>
-  <title>MADComs</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
   <link rel="stylesheet" href="css/styles.css">
+  <title>MADComms</title>
 </head>
 
 <body>
-  <nav class="navbar navbar-expand-md navbar-dark">
+  <nav class="navbar navbar-expand-md navbar-dark sticky-top bg-dark">
     <div class="container">
       <a class="navbar-brand">MADComms</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -48,13 +50,13 @@
       </div>
       <div class="carousel-inner">
         <div class="carousel-item active">
-          <img src="./images/img1.jpg" class="d-block w-100">
+          <img src="./images/img1.jpg" class="d-block w-100" alt="...">
         </div>
         <div class="carousel-item">
-          <img src="./images/img2.jpg" class="d-block w-100">
+          <img src="./images/img2.jpg" class="d-block w-100" alt="...">
         </div>
         <div class="carousel-item">
-          <img src="./images/img3.jpg" class="d-block w-100">
+          <img src="./images/img3.jpg" class="d-block w-100" alt="...">
         </div>
       </div>
       <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
@@ -68,13 +70,85 @@
     </div>
   </div>
 
-  <div id="sprendimai"></div>
-  <div id="verslo-sritys"></div>
-  <div id="apie-mus"></div>
-  <div id="kontaktai"></div>
+  <div id="sprendimai">
+  </div>
+  <div id="verslo-sritys">
+  </div>
+  <div id="apie-mus" class="container">
+  </div>
+  <div id="kontaktai" class="container">
+    <div class="row">
+      <div class="col-sm-12 text-center pt-5">
+        <h1 class="text-white">KONTAKTAI</h1>
+        <p class="text-white">Džiaugsimės sulaukę Jūsų skambučio, el. laiško ar užpildytos kontaktų formos!</p>
+      </div>
+      <div class="col-sm-12 col-xl-6 pt-5 mt-2">
+        <div class="contact-form">
+          <h3 class="text-center text-white">Susisiekite su mumis!</h3>
+          <form class="pt-5">
+            <div class="mb-3">
+              <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Vardas Pavardė">
+            </div>
+            <div class="mb-3">
+              <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="El. paštas">
+            </div>
+            <div class="mb-3">
+              <input type="phone" class="form-control" id="exampleFormControlInput1" placeholder="Telefono numeris">
+            </div>
+            <div class="mb-3">
+              <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="Įveskite žinutę"></textarea>
+            </div>
+            <input class="btn btn-light" type="submit" value="Siųsti">
+          </form>
+        </div>
+      </div>
+      <div class="col-sm-12 col-xl-6 pt-5 mt-2">
+        <h3 class="text-center text-white">Kontaktinė informacija</h3>
+        <div class="ps-5 pt-5 text-start">
+          <p class="text-white">UAB "MADComms"</p>
+          <p class="text-white">Biuro adresas: Didlaukio g. 47, Vilnius LT-06229</p>
+          <p class="text-white">Registracijos adresas: Didlaukio g. 47, Vilnius LT-06229</p>
+          <p class="text-white">Telefonas: 8 5 973 2977</p>
+          <p class="text-white">El. p.: info@madcomms.com</p>
+        </div>
+      </div>
+    </div>
+  </div>
 
   <a href="https://wa.me/8569541244" class="whatsapp_float" target="_blank"><i class="fa fa-whatsapp whatsapp-icon"></i></a>
+
+  <footer id="footer" class="bg-dark">
+    <div class="container">
+      <div class="row pt-5 mt-5 pb-5">
+        <div class="col-sm-12 col-xl-8">
+          <h4 class="text-white">
+            Kontaktai
+          </h4>
+          <div class="pt-5 text-start">
+            <p class="text-white">UAB "MADComms"</p>
+            <p class="text-white">Biuro adresas: Didlaukio g. 47, Vilnius LT-06229</p>
+            <p class="text-white">Registracijos adresas: Didlaukio g. 47, Vilnius LT-06229</p>
+            <p class="text-white">Telefonas: 8 5 973 2977</p>
+            <p class="text-white">El. p.: info@madcomms.com</p>
+          </div>
+        </div>
+        <div class="col-sm-12 col-xl-4">
+          <h4 class="text-white">
+            Bendraukime
+          </h4>
+          <div id="socials" class="pt-5">
+            <a href="#" class="pe-1"><img src="images/fb-icon.png" alt=""></a>
+            <a href="#" class="pe-1"><img src="images/twitter-icon.png" alt=""></a>
+            <a href="#"><img src="images/linkedin-icon.png" alt=""></a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+
 </body>
 
 </html>
